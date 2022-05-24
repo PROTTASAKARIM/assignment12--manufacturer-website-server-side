03529923dddd29f6d35d45c2951f117b6b664fa1
PROTTASAKARIM <prottasakarim99@gmail.com>
2022-05-24 14:38:21 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.kdux9.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('manufacturer').collection('products');
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query).project();
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+
+
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello From Doctor Uncle own portal!')
+})
+
+app.listen(port, () => {
+    console.log(`Doctors App listening on port ${port}`)
+})
